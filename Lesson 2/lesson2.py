b521d4a39631bcfe8a830dfcc7598c5115954f13
lavedon <lavedon@gmail.com>
2017-09-22 11:55:45 -0400

    updated.

--- Lesson 2/lesson2.py
+++ Lesson 2/lesson2.py
@@ -1,1 +1,14 @@
 import pygame
+import libtcodpy
+
+def game_main_loop():
+
+    game_quit = False
+    
+    while True:
+
+        #TODO get player input
+
+        #TODO process input
+
+        #TODO draw the game
