f530eb0a9d44f9ee07745bbd404ee44550cf9ffe
lavedon <lavedon@gmail.com>
2017-09-23 17:54:19 -0400

    Lesson 4. Move hero. Don't bounce into walls.

---
+++ Lesson 4/constants.py
@@ -0,0 +1,39 @@
+import pygame
+
+pygame.init()
+
+#   ____      _
+#  / ___|___ | | ___  _ __ ___
+# | |   / _ \| |/ _ \| '__/ __|
+# | |__| (_) | | (_) | |  \__ \
+#  \____\___/|_|\___/|_|  |___/
+
+# Game sizes
+GAME_WIDTH = 800
+GAME_HEIGHT = 600
+CELL_WIDTH = 32
+CELL_HEIGHT = 32
+
+#MAP VARS
+MAP_WIDTH = 30
+MAP_HEIGHT = 30
+
+# Color definitions
+COLOR_BLACK = (0, 0, 0)  # RGB for black
+COLOR_WHITE = (255, 255, 255)  # RGB for white
+COLOR_GREY = (100, 100, 100)
+
+# Game colors
+COLOR_DEFAULT_BG = COLOR_GREY
+
+#  ____             _ _
+# / ___| _ __  _ __(_) |_ ___  ___
+# \___ \| '_ \| '__| | __/ _ \/ __|
+#  ___) | |_) | |  | | ||  __/\__ \
+# |____/| .__/|_|  |_|\__\___||___/
+#       |_|
+
+#character
+S_PLAYER = pygame.image.load('data/python.png')
+S_WALL = pygame.image.load('data/wall.png')
+S_FLOOR = pygame.image.load('data/floor.jpg')
